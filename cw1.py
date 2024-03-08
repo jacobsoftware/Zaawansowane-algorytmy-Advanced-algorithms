1a39acca1b7f07ac346d1d00204f8d0da4d369a5
jacobSoftware <jacob.weresoft@gmail.com>
2024-03-08 01:59:07 +0100

    stack

--- cw1.py
+++ cw1.py
@@ -3,12 +3,8 @@ class Node:
         self.value = value
         self.nextNode = None
         self.prevNode = None
-        
-      
+              
 
-# self.head - wskaźnik na pierwszy element listy o ile lista nie jest pusta, aby uzyskać wartość dla tego wskażnika 
-# trzeba dodatkowo skorzystać z value (self.head.value)
-# nextNode pozwala nam przejśc do kolejnego wskaźnika z listy
 class List:
 
     def __init__(self):
@@ -32,32 +28,41 @@ class List:
     def write(self):
         node = self.head
         string = '//'
+
         while node:           
             string = string +  str(node.value) + ' '
-            node = node.nextNode           
+            node = node.nextNode 
+
         print(string)
 
     def list_search(self,key):
         node = self.head
         if_empty = True
+
         while node:
             if node.value == key:
                 if_empty = False
                 print(node.value)
+
             node = node.nextNode
+
         if if_empty:
             print('// -1')
 
     def list_insert_after(self,key,after):
         node = self.head
+
         while node:
             if node.value == after:
                 new_node = Node(key)
                 new_node.nextNode = node.nextNode
+
                 if node.nextNode:
                     node.nextNode.prevNode = new_node
+
                 else:
                     self.tail = new_node
+
                 node.nextNode = new_node
                 new_node.prevNode = node
 
@@ -65,8 +70,10 @@ class List:
 
     def list_delete(self,key):
         node = self.head
+
         while node:
             if node.value == key:
+
                 if node.prevNode:
                     node.prevNode.nextNode = node.nextNode
 
@@ -85,16 +92,21 @@ class List:
 
     def list_insert_before(self,key,before):
         node = self.head
+
         while node:
             if node.value == before:
                 new_node = Node(key)
                 new_node.prevNode = node.prevNode
+
                 if node.prevNode:
                     node.prevNode.nextNode = new_node
+                    
                 else:
                     self.head = new_node
+
                 node.prevNode = new_node
                 new_node.nextNode = node
+
             node = node.nextNode
 
     
@@ -103,10 +115,6 @@ class List:
         self.tail = None
         return print('/*zwalnia pamięć*/')
     
-    def test(self):
-        print('\nTestowanie, funkcja spoza tematu zadania:')
-        print('Test Head:',self.head.value)
-        print('Test Tail:',self.tail.value)
 
 def zadanie1():
     L = List()
@@ -122,19 +130,68 @@ def zadanie1():
     L.list_delete(2)
     L.write()
     L.list_delete(5)
+    L.write()
     L.list_insert_before(5,3)
     L.write()
     L.clear()
     L.write()
-    #L.test()
+
+
+class Stack():
+    def __init__(self):
+        self.data = List()
+        self.top = 0 
+
+    def stack_empty(self):
+        if self.top is 0: print('true')
+        else: print('false')
+    
+    def push(self,value):
+        self.data.list_insert(value)
+        self.top = self.top + 1
+
+    def pop(self):
+        if self.top is not 0:
+            top_value = self.data.head.value
+            string = '//'+str(top_value)
+            print(string)
+            self.data.list_delete(top_value)
+            self.top = self.top - 1
+        else:
+            print('//niedomiar')
+
+def zadanie2():  
+    S = Stack()
+    S.push(1)
+    S.push(2)
+    S.data.write()
+    S.push(3)
+    S.data.write()
+    S.stack_empty()
+    S.pop()
+    S.pop()
+    S.data.write()
+    S.pop()
+    S.pop()
+    S.data.write()
+    S.stack_empty()
+
+class Queue():
+    def __init__(self):
+        self.data = List()
+        self.length = 10
+        self.head = 1
+        self.tail = 1
+
+def zadanie3():
+    Q = Queue()
 
 if __name__ == '__main__':
     print('Zadanie 1: \n')
     zadanie1()
-    # print('\nZadanie 2: \n')
+    print('\nZadanie 2: \n')
+    zadanie2()
+    print('\nZadanie 3: \n')
+    zadanie3()
 
-    # print('\nZadanie 3: \n')
-
-
-    
 
